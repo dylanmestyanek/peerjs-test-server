9464272efb418678b9b68afdce390c4b19f856a7
dylanmestyanek <mestyanekdylan@gmail.com>
2020-06-17 10:51:35 -0500

    progress

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 const { ExpressPeerServer } = require("peer");
 
 const app = express();
 
 const server = app.listen(process.env.PORT);
 
-app.get("/", (req, res) => {
-  res.sendStatus(200);
+let host = null;
+
+let connections = [];
+const peerServer = ExpressPeerServer(server);
+
+peerServer.on("connection", ({ id }) => {
+  if (!host) {
+    host = id;
+  }
+
+  connections = [id, ...connections];
 });
 
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
+peerServer.on("disconnect", ({ id }) => {
+  connections = connections.filter((id) => connections.indexOf(id) !== 1);
+  if (host === id) {
+    host = connections[0];
+  }
 });
 
-peerServer.on("connection", console.log);
-peerServer.on("disconnect", console.log);
+app.use(cors());
+app.use("/", peerServer);
 
-app.use("/peerjs", peerServer);
+app.get("/peerjs/host", (req, res) => {
+  res.status(200).send({ host, connections });
+});
